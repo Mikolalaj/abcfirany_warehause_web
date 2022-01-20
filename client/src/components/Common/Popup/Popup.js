8f26df872ca754b110931a6eee77781f877d06fa
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2022-01-20 20:05:37 +0100

    updated popup closing

--- client/src/components/Common/Popup/Popup.js
+++ client/src/components/Common/Popup/Popup.js
@@ -1,17 +1,21 @@
 import './Popup.css'
+import { useState } from 'react';
 
 function Popup({ children, trigger, closePopup }) {
 
+    const [startClick, setStartClick] = useState(null);
+
     function handleClickOutside(event) {
         event.preventDefault();
-        if(event.target === event.currentTarget) {
+        if(startClick === event.currentTarget) {
             closePopup();
         }
+        setStartClick(null);
     }
-
+    
     return (
         trigger ? (
-        <div className='popup' onClick={handleClickOutside}>
+        <div className='popup' onClick={handleClickOutside} onMouseDown={event => setStartClick(event.target)}>
             <div className="popup-inner">
                 <div className="popup-content">
                     {children}
